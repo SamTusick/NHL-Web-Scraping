afc70ac466083d0f8ee32fbbb48a2a37441a463d
SamTusick <stusick@outlook.com>
2025-06-01 01:51:49 -0400

    Created functions for Assists and points

--- app.py
+++ app.py
@@ -3,20 +3,53 @@
 # app.py
 from flask import Flask, jsonify, request
 from scraper.driver import get_driver
-from scraper.scraper_functions import scrape_goal_leaders  
-from scraper.scraper_functions import close_cookie_window  
+from scraper.scraper_functions import (
+     scrape_goal_leaders,
+     scrape_assist_leaders,
+     scrape_points_leaders,  
+     close_cookie_window  
+)
 
 app = Flask(__name__)
+
+
+# Main Page
 
 @app.route("/")
 def index():
     return jsonify({"message": "Welcome to the NHL Stats API!"})
 
+# Goal Leaders
+
 @app.route("/stats/goals", methods=["GET"])
 def get_goal_stats():
+
     count = request.args.get("count", default=5, type=int)
 
     count = 6
+    
+    driver, wait = get_driver()
+    driver.get("https://www.nhl.com")
+    driver.maximize_window()
+
+    try:
+        close_cookie_window(driver, wait)
+        data = scrape_goal_leaders(driver, wait, count, is_first=True)
+
+
+        return jsonify(data)
+    finally:
+        driver.quit()
+
+# Assists Leaders
+
+@app.route("/stats/assists", methods=["GET"])
+def get_assists_stats():
+
+    count = request.args.get("count", default=5, type=int)
+
+    count = 6
+
 
     driver, wait = get_driver()
     driver.get("https://www.nhl.com")
@@ -24,7 +57,32 @@ def get_goal_stats():
 
     try:
         close_cookie_window(driver, wait)
-        data = scrape_goal_leaders(driver, wait, count, is_first=True) 
+        data = scrape_assist_leaders(driver, wait, count, is_first=True)
+
+
+        return jsonify(data)
+    finally:
+        driver.quit()
+
+# Point Leaders
+
+@app.route("/stats/points", methods=["GET"])
+def get_points_stats():
+
+    count = request.args.get("count", default=5, type=int)
+
+    count = 6
+
+
+    driver, wait = get_driver()
+    driver.get("https://www.nhl.com")
+    driver.maximize_window()
+
+    try:
+        close_cookie_window(driver, wait)
+        data = scrape_points_leaders(driver, wait, count, is_first=True)
+
+
         return jsonify(data)
     finally:
         driver.quit()
--- scraper/scraper_functions.py
+++ scraper/scraper_functions.py
@@ -9,9 +9,19 @@ import datetime
 
 now = datetime.datetime.now().strftime("%B %d, %Y at %I:%M %p")
 
+# Wait for Spinner to Close
+
+#def wait_for_spinner_to_disappear(wait):
+    #try:
+ #       wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, "loading-container")))
+  #  except Exception as e:
+ #       print("Spinner may not be present or already hidden:", e)
+
+# Close Cookie Window
+
 def close_cookie_window(driver, wait):
     try:
-        #checks to see if cookie banner is present
+        # Checks to see if cookie banner is present
         cookieAccept = driver.find_element(By.XPATH, "//*[@id='onetrust-accept-btn-handler']")
         cookieAccept.click()
         print("\nCookie window close successfully!\n")
@@ -19,13 +29,23 @@ def close_cookie_window(driver, wait):
     except Exception as e:
         print("\nError finding or closing cookie banner: " , e, "\n") 
 
+# Click Stat Tab
+
 def click_stat_tab(driver, wait):
+
+    wait_for_spinner_to_disappear(wait)
     statsElement = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Stats')]")))
     statsElement.click()
 
+# Click Skater Section
+
 def click_skater_section(driver, wait):
+
+    wait_for_spinner_to_disappear(wait)
     skater_tab = wait.until(EC.element_to_be_clickable((By.ID, "skaters")))
     skater_tab.click()
+
+# Goal Leaders Function
 
 def scrape_goal_leaders(driver, wait, count=5, is_first=True):
     print("Clicking Stat Tab...")
@@ -70,16 +90,18 @@ def scrape_goal_leaders(driver, wait, count=5, is_first=True):
         "leaders": leaders
     }
 
-def scrape_assist_leaders(driver, wait, count=5, is_First= True):
+# Assist Leaders Function
+
+def scrape_assist_leaders(driver, wait, count=5, is_first= True):
     print("Clicking Stat Tab...")
     click_stat_tab(driver, wait)
 
     print("Getting Skaters...")
     click_skater_section(driver, wait)
 
-    print("Clicking Goals column...")
+    print("Clicking Assists column...")
 
-    click_goal = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@title='Goals']")))
+    click_goal = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@title='Assists']")))
     wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='season-tabpanel']//table")))
 
     scroll_origin = ScrollOrigin.from_element(click_goal)
@@ -92,10 +114,10 @@ def scrape_assist_leaders(driver, wait, count=5, is_First= True):
     click_times = 3 if is_first else 1
     for _ in range(click_times):
     # Re-select the element each time in case the DOM updates
-        click_goal = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@title='Goals']")))
+        click_goal = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@title='Assists']")))
         click_goal.click()
     # Used for debugging
-        print(f"Clicking goals column... ({_+1}/{click_times})")
+        print(f"Clicking Assists column... ({_+1}/{click_times})")
         time.sleep(0.5)
 
     # Grab the top N rows
@@ -105,10 +127,55 @@ def scrape_assist_leaders(driver, wait, count=5, is_First= True):
     leaders = []
     for i in range(min(count, len(rows))):
         name = rows[i].find_element(By.XPATH, "./td[2]/div/a").text
-        goals = rows[i].find_element(By.XPATH, "./td[8]").text
-        leaders.append({"name": name, "goals": goals})
+        assists = rows[i].find_element(By.XPATH, "./td[9]").text
+        leaders.append({"name": name, "assists": assists})
 
     return {
         "title": f"Top {count} Assist Leaders (As of {now})",
+        "leaders": leaders
+    }
+
+# Points Leaders Function
+
+def scrape_points_leaders(driver, wait, count=5, is_first= True):
+    print("Clicking Stat Tab...")
+    click_stat_tab(driver, wait)
+
+    print("Getting Skaters...")
+    click_skater_section(driver, wait)
+
+    print("Clicking Points column...")
+
+    click_points = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@title='Points']")))
+    wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='season-tabpanel']//table")))
+
+    scroll_origin = ScrollOrigin.from_element(click_points)
+    ActionChains(driver)\
+        .scroll_to_element(click_points)\
+        .scroll_from_origin(scroll_origin, 0, 200)\
+        .perform()
+    time.sleep(0.5)
+
+    click_times = 3 if is_first else 1
+    for _ in range(click_times):
+    # Re-select the element each time in case the DOM updates
+        click_points = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@title='Points']")))
+        click_points.click()
+    # Used for debugging
+        print(f"Clicking Points column... ({_+1}/{click_times})")
+        time.sleep(0.5)
+
+    # Grab the top N rows
+    rows = driver.find_elements(By.XPATH, "//*[@id='season-tabpanel']//table/tbody/tr")
+
+    
+    leaders = []
+    for i in range(min(count, len(rows))):
+        name = rows[i].find_element(By.XPATH, "./td[2]/div/a").text
+        points = rows[i].find_element(By.XPATH, "./td[10]").text
+        leaders.append({"name": name, "points": points})
+
+    return {
+        "title": f"Top {count} Points Leaders (As of {now})",
         "leaders": leaders
     }
