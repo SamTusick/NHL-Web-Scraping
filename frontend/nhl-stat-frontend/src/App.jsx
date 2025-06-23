89ba199ad60588f64c965ddc7f7c65bdc381effb
SamTusick <stusick@outlook.com>
2025-06-22 21:26:34 -0400

    Updated landing page

--- frontend/nhl-stat-frontend/src/App.jsx
+++ frontend/nhl-stat-frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import '../src/styling/App.css'
+import StartButton from './components/StartButton';
+
 
 export default function App() {
   return (
-    <div>
-      <h1>NHL Stat Web Scraper</h1>
-
-    </div>
+    <>
+      <div>
+        <h1 className='title'>NHL Stat Web Scraper</h1>
+      </div>
+      <StartButton></StartButton>
+    </>
   );
 }
---
+++ frontend/nhl-stat-frontend/src/components/StartButton.jsx
@@ -0,0 +1,11 @@
+/* Start Button Component */
+
+export default function StartButton(){
+    return (
+    <div className="start-button-wrapper">
+        <button className="start-button">
+            Get Started
+        </button>
+    </div>
+    )
+}
