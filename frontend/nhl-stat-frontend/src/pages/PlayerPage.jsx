5e16881446143062343b1ed5671ce4d63bf9ff72
SamTusick <stusick@outlook.com>
2025-06-27 23:11:31 -0400

    Started to create player selection page

---
+++ frontend/nhl-stat-frontend/src/pages/PlayerPage.jsx
@@ -0,0 +1,16 @@
+/* Player Page */
+
+import '../styling/playerpage.css'
+
+export default function PlayerPage(){
+    return (
+        <>
+            <div className="player-container">
+                <div className="player-content">
+                    <h1 className='title'>Player Selection</h1>
+                    <h1 className='sub-title'>Select The Type of Player You Want To See</h1>
+                </div>
+            </div>
+        </>
+    )
+}
