2e95715f474342381808b2cd2f8937c7560eae58
SamTusick <stusick@outlook.com>
2025-06-29 13:53:59 -0400

    Updated Home Page Get Started Button to line up with other pages

--- frontend/nhl-stat-frontend/src/pages/Home.jsx
+++ frontend/nhl-stat-frontend/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 /* Home Page */
 import SelectionButton from '../components/SelectionButton'
-import StartButton from '../components/StartButton'
 import '../styling/home.css'
 
 export default function HomePage(){
@@ -8,6 +7,9 @@ export default function HomePage(){
         <div className="home-container">
             <div className="home-content">
                 <h1 className='title'>NHL Stat Web Scraper</h1>
+                <h4 className='sub-title'></h4> 
+                <h4 className='sub-title'></h4> 
+                <h4 className='sub-title'></h4> 
                 <div className='selection-button-center'>
                     <div className='selection-button-wrapper'>
                         <SelectionButton label="Get Started" to="/PlayerPage" />
