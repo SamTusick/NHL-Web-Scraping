0993199b1915874bd45e64a6ceaca9ac65697c2d
SamTusick <stusick@outlook.com>
2025-07-02 21:06:48 -0400

    Updated chrome driver for Render

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def setup_driver():
     options.add_argument("--ignore-certificate-errors")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
