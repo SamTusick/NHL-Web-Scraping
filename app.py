91035144509a81c950f65216e64ac0fccf56a8e2
SamTusick <stusick@outlook.com>
2026-01-09 00:32:09 -0500

    Updating to work for API

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ def setup_driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-blink-features=AutomationControlled")   
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 15)
