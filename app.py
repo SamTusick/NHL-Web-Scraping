04ab4271106118fb50391f42ce293ac8a5bc92c4
SamTusick <stusick@outlook.com>
2026-01-09 00:13:41 -0500

    Update selenium options

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from scraper.scraper_functions import (
